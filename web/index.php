3a7166d1ff146b644fa1dbe74732f94f9bd06470
Dianamarlen12 <1718110399@utectulancingo.edu.mx>
2021-06-27 04:49:52 +0000

    nuevo

---
+++ web/index.php
@@ -0,0 +1,80 @@
+<html>
+  <title>FORMULARIO</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> 
+    <style type="text/css">
+        body { background-color: rgba(102, 183, 230, 0.637); }
+        header{
+    width: 100%;
+    padding: 0px;
+    color: white;
+    text-align: center;
+}
+nav ul{
+    width: 100%;
+    background-color: rgb(81, 176, 231);
+    color: rgb(255, 254, 254);
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    letter-spacing: normal;
+    overflow: hidden;
+    -webkit-transition: max-height 0.4s;
+    -ms-transition: max-height 0.4s;
+    -moz-transition: max-height 0.4s;
+    -o-transition: max-height 0.4s;
+    transition: max-height 0.4s;
+}
+/*BARRA PEQUEÑA DEL TITULO*/
+nav ul li{
+    display: inline-block;
+    padding: 15px;
+}
+
+a{
+    text-decoration: none;
+    color: inherit;
+    display: absolute;
+}
+section{
+    line-height: 1.5em;
+    font-size: 0.9em;
+    padding: 10px;
+    margin: 0 auto;
+    max-width: 960px;
+}
+      </style>
+      
+    <body>
+        <header>
+            <br>
+          </header>
+          <nav>
+            <ul>
+              <li class="nav-item">
+                <a href="/list"><li><h4>LISTA</h4></li></a>
+                <a href="/insert"><li><h4>INSERTAR</h4></li></a>
+            </li>
+            </ul>
+          </nav>
+    <section>      
+    <center>
+        <h1>BIENVENIDO</h1>
+        <h2>Este es un formulario que tu debes de llenar</h2>
+        <h3>Debes ser muy claro con los datos y los datos deben ser verdaderos</h3>
+        <img src="https://image.freepik.com/vector-gratis/plantilla-formulario-registro-diseno-plano_23-2147971970.jpg"
+        width="400"
+        height="341">
+    </center>
+</section>
+    </body>
+</html>
---
+++ web/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE personas(
+    nombre varchar(50),
+    email varchar(50),
+    telefono varchar(10)
+);
+
+INSERT INTO personas(nombre,email,telefono)
+VALUES
+('Diana','marlen@gmail.com',7751170830);
